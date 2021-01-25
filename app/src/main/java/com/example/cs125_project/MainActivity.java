159efe0b6d9dc6457d589aecce6e9bf772f30ccf
Lillian Won <lillian.won@live.com>
2021-01-24 20:36:48 -0800

    Added Sleep Button to make menu

--- app/src/main/java/com/example/cs125_project/MainActivity.java
+++ app/src/main/java/com/example/cs125_project/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.cs125_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button sleepBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sleepBtn = (Button) findViewById(R.id.sleepBtn);
+        sleepBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openSleepActivity();
+            }
+        });
+    }
+
+    public void openSleepActivity() {
+        Intent i = new Intent(this, sleep_activity.class);
+        startActivity(i);
     }
 }
