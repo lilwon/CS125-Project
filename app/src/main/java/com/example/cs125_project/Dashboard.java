a26c7c69052b8fccdb086e22dfa7c268648c29ff
Lillian Won <lillian.won@live.com>
2021-02-07 00:02:31 -0800

    Moved activities to a new fragment

--- app/src/main/java/com/example/cs125_project/Dashboard.java
+++ app/src/main/java/com/example/cs125_project/Dashboard.java
@@ -1,17 +1,31 @@
 package com.example.cs125_project;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dashboard extends AppCompatActivity {
 
     DrawerLayout dl;
     private Button enterSleepActivity;
+
+    TabLayout tab;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +34,73 @@ public class Dashboard extends AppCompatActivity {
 
         dl = findViewById(R.id.drawer_layout);
 
-        enterSleepActivity = (Button) findViewById(R.id.enterSleepActivity);
-        enterSleepActivity.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                openSleepActivity();
-            }
-        });
+        // For Tab Layout Fragments
+        tab = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        ArrayList<String> tabArray = new ArrayList<>();
+        tabArray.add("Main");
+        tabArray.add("Graph");
+
+        prepViewPager(viewPager, tabArray);
+
+       tab.setupWithViewPager(viewPager);
 
     }
 
-    public void openSleepActivity() {
-        Intent i = new Intent(this, sleep_activity.class);
-        startActivity(i);
+    private void prepViewPager(ViewPager viewPager, ArrayList<String> tabArray) {
+        PageAdapter adapter = new PageAdapter(getSupportFragmentManager());
+
+        MainFragment frag = new MainFragment();
+
+        for (int i = 0; i < tabArray.size(); i++) {
+            Bundle bundle= new Bundle();
+            bundle.putString("title", tabArray.get(i) );
+
+            frag.setArguments(bundle);
+
+            adapter.addFragment(frag, tabArray.get(i));
+
+            frag = new MainFragment();
+        }
+
+        // Set Page adapter
+        viewPager.setAdapter(adapter);
+
     }
+
+    private class PageAdapter extends FragmentPagerAdapter {
+        ArrayList<String> tempList = new ArrayList<>();
+        List<Fragment> fragmentList = new ArrayList<>();
+
+        public void addFragment(Fragment frag, String fragName) {
+            tempList.add(fragName);
+            fragmentList.add(frag);
+        }
+
+        public PageAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tempList.get(position);
+        }
+    }
+
 
     public void ClickMenu(View view) {
         MainActivity.openDrawer(dl);
