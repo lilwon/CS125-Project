4dbd4b25aa55f1c8b5b3459b3512341595c9b936
Vivian Nham <vnham@uci.edu>
2021-02-15 18:41:01 -0800

    Added back button in sleep activity page
    Signed-off-by: Vivian Nham <vnham@uci.edu>

--- app/src/main/java/com/example/cs125_project/sleep_activity.java
+++ app/src/main/java/com/example/cs125_project/sleep_activity.java
@@ -12,6 +12,7 @@ import android.widget.TimePicker;
 public class sleep_activity extends AppCompatActivity {
 
     private Button next_button;
+    private Button back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +20,13 @@ public class sleep_activity extends AppCompatActivity {
         setContentView(R.layout.activity_sleep_activity);
 
         TimePicker simpleTimePicker=(TimePicker)findViewById(R.id.simpleTimePicker); //initiate a time picker
-
+        back_button = (Button) findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backto_home();
+            }
+        });
         next_button = (Button) findViewById(R.id.next_button);
         next_button.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -31,6 +38,11 @@ public class sleep_activity extends AppCompatActivity {
 
     public void openSleepActivity2() {
         Intent i = new Intent(this, sleep_activity2.class);
+        startActivity(i);
+    }
+
+    public void backto_home(){
+        Intent i = new Intent(this, Dashboard.class);
         startActivity(i);
     }
 
