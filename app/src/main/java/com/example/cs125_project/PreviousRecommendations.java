9e849b558da2bf8a9b641712cb3abe89f21809f2
Lillian Won <lillian.won@live.com>
2021-03-06 17:39:55 -0800

    Fixed other activity layouts and added DL to each of them

--- app/src/main/java/com/example/cs125_project/PreviousRecommendations.java
+++ app/src/main/java/com/example/cs125_project/PreviousRecommendations.java
@@ -1,12 +1,16 @@
 package com.example.cs125_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -22,16 +26,50 @@ public class PreviousRecommendations extends AppCompatActivity {
     String userid = user.getUid();
     DatabaseReference reff = FirebaseDatabase.getInstance().getReference().child("Users").child(userid);
 
+    // Included a DrawerLayout so must add DL code
+    DrawerLayout dl;
+
+    private Button backBtn;
 
     @Override   protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_previous_recommendations_main);
 
-
+        dl = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         simpleList = (ListView)findViewById(R.id.prev_recs);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_previous_recommendations, R.id.textView, mobileArray);
         simpleList.setAdapter(arrayAdapter);
+
+        backBtn = (Button) findViewById(R.id.back_button);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Return to Dashboard
+                openDashboard();
+            }
+        });
     }
 
+    // Drawer Layout navigation..
+    public void ClickMenu(View view) {
+        MainActivity.openDrawer(dl);
+    }
+
+    public void ClickLogo(View view) {
+       MainActivity.closeDrawer(dl);
+    }
+
+    public void ClickHome(View view) {
+        MainActivity.redirectActivity(this, Dashboard.class);
+    }
+
+    public void ClickDashBoard(View view) {
+        MainActivity.redirectActivity(this, Dashboard.class);
+    }
+
+    public void openDashboard() {
+        Intent i = new Intent(this, Dashboard.class);
+        startActivity(i);
+    }
 }
--- app/src/main/java/com/example/cs125_project/UserInfo.java
+++ app/src/main/java/com/example/cs125_project/UserInfo.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,22 +18,30 @@ import com.google.firebase.database.ValueEventListener;
 
 public class UserInfo extends AppCompatActivity  {
 
-    private Button user_info_next_button;
+    //private Button user_info_next_button;
     private Button user_info_back_button;
 
+    // If there's a DL you need to add the code for it.
+    DrawerLayout dl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
 
-        user_info_next_button = (Button) findViewById(R.id.user_info_next_button);
+        // Don't need a next button.
+        //user_info_next_button = (Button) findViewById(R.id.user_info_next_button);
+        /*
         user_info_next_button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 openSleepActivity2();
             }
         });
+        */
+
+        // Activate the drawer layout
+        dl = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         user_info_back_button = (Button) findViewById(R.id.user_info_back_button);
         user_info_back_button.setOnClickListener(new View.OnClickListener(){
@@ -46,6 +55,21 @@ public class UserInfo extends AppCompatActivity  {
     public void openSleepActivity2() {
         Intent i = new Intent(this, sleep_activity2.class);
         startActivity(i);
+    }
+
+    public void ClickMenu(View view) {
+        MainActivity.openDrawer(dl);
+    }
+
+    public void ClickLogo(View view) {
+        MainActivity.closeDrawer(dl);
+    }
+    public void ClickHome(View view) {
+        MainActivity.redirectActivity(this, Dashboard.class);
+    }
+
+    public void ClickDashboard(View view) {
+        MainActivity.redirectActivity(this, Dashboard.class);
     }
 
     public void openDashboard() {
