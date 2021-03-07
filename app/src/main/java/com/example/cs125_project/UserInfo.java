6b86577409525334841b3dc5169d73c538a533d3
Lillian Won <lillian.won@live.com>
2021-03-06 20:42:51 -0800

    Saving more user info to database

--- app/src/main/java/com/example/cs125_project/UserInfo.java
+++ app/src/main/java/com/example/cs125_project/UserInfo.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,6 +26,12 @@ public class UserInfo extends AppCompatActivity  {
 
     // If there's a DL you need to add the code for it.
     DrawerLayout dl;
+
+    // Writing info to Firebase
+    private DatabaseReference db;
+
+    private EditText avgHrText;
+    private EditText idealHrText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +51,20 @@ public class UserInfo extends AppCompatActivity  {
 
         // Activate the drawer layout
         dl = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // Get average amt of sleep user slept and their ideal amt of sleep
+        avgHrText = (EditText) findViewById(R.id.avg_hour_sleep);
+        idealHrText = (EditText) findViewById(R.id.ideal_sleep);
+
+        // convert to Int
+        String avgHr = avgHrText.getText().toString();
+        String idealHr = idealHrText.getText().toString();
+
+        int avgHrVal = Integer.parseInt(avgHr);
+        int idealHrVal = Integer.parseInt(idealHr);
+
+        // Store vals to DB
+        storeToDatabase(avgHrVal, idealHrVal);
 
         user_info_back_button = (Button) findViewById(R.id.user_info_back_button);
         user_info_back_button.setOnClickListener(new View.OnClickListener(){
@@ -77,4 +100,15 @@ public class UserInfo extends AppCompatActivity  {
         startActivity(i);
     }
 
+    public void storeToDatabase(int avgHr, int idealHr) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String useruid = user.getUid();
+
+        db = FirebaseDatabase.getInstance().getReference();
+
+        // Create a new Node on database..
+        db.child("Users").child(useruid).child("goals").child("currentAvg").setValue(avgHr);
+        db.child("Users").child(useruid).child("goals").child("idealHr").setValue(idealHr);
+
+    }
 }
