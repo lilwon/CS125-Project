5cff5a4ec1b1cba1f2353be1f734779599e653b1
Kayla Yee <yeekj@uci.edu>
2021-03-08 15:44:41 -0800

    adding prev_recs to ListView
    partially completed, need to fix - it's not showing the data right now for some reason

--- app/src/main/java/com/example/cs125_project/PreviousRecommendations.java
+++ app/src/main/java/com/example/cs125_project/PreviousRecommendations.java
@@ -3,28 +3,36 @@ package com.example.cs125_project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.List;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class PreviousRecommendations extends AppCompatActivity {
     // Array of strings...
     ListView simpleList;
-    String mobileArray[] = {"sleep rec1", "sleep rec2", "sleep rec3", "sleep rec4", "sleep rec5", "sleep rec6"};
+    ArrayList<String> previous_rec_list  = new ArrayList<String>();
+    //String mobileArray[] = {"sleep rec1", "sleep rec2", "sleep rec3", "sleep rec4", "sleep rec5", "sleep rec6"};
 
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     String userid = user.getUid();
@@ -44,15 +52,41 @@ public class PreviousRecommendations extends AppCompatActivity {
 
     private Button backBtn;
 
-    @Override   protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_previous_recommendations_main);
 
-        dl = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        simpleList = (ListView)findViewById(R.id.prev_recs);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_previous_recommendations, R.id.textView, mobileArray);
-        simpleList.setAdapter(arrayAdapter);
+        // READ DATA FROM FIREBASE
+        userRef.addValueEventListener(new ValueEventListener() {
+
+            Integer best_rec;
+            Integer better_rec;
+            Integer mod_rec;
+
+            String currentDate = getDateTime();
+
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    // ds.child("hourSlept").child(currentDate).child("best_rec").getValue())
+                    best_rec = ds.child("hourSlept").child(currentDate).child("best_rec").getValue(Integer.class);
+                    better_rec = ds.child("hourSlept").child(currentDate).child("better_rec").getValue(Integer.class);
+                    previous_rec_list.add(String.valueOf(best_rec));
+                    previous_rec_list.add(String.valueOf(better_rec));
+                    Log.v("best rec ", String.valueOf(previous_rec_list.get(1)));
+                    Log.v("better rec ", String.valueOf(previous_rec_list.get(1)));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+
+        });
+
+        dl = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         backBtn = (Button) findViewById(R.id.back_button);
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -62,6 +96,10 @@ public class PreviousRecommendations extends AppCompatActivity {
                 openDashboard();
             }
         });
+
+        simpleList = (ListView) findViewById(R.id.prev_recs);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_previous_recommendations, R.id.textView, previous_rec_list);
+        simpleList.setAdapter(arrayAdapter);
     }
 
     // Drawer Layout navigation..
@@ -70,7 +108,7 @@ public class PreviousRecommendations extends AppCompatActivity {
     }
 
     public void ClickLogo(View view) {
-       MainActivity.closeDrawer(dl);
+        MainActivity.closeDrawer(dl);
     }
 
     public void ClickHome(View view) {
@@ -87,7 +125,7 @@ public class PreviousRecommendations extends AppCompatActivity {
     }
 
     // get month and year for Firebase db
-    private String getYear () {
+    private String getYear() {
         DateFormat dateFormat = new SimpleDateFormat("yyyy");
         Date yr = new Date();
         return dateFormat.format(yr);
@@ -98,4 +136,12 @@ public class PreviousRecommendations extends AppCompatActivity {
         Date month = new Date();
         return dateFormat.format(month);
     }
+
+
+    private String getDateTime() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
 }
