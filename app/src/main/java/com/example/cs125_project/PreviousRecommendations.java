dff1a0e396c70a3d387f82eb14df602c2c6a99e5
Kayla Yee <yeekj@uci.edu>
2021-03-08 20:12:44 -0800

    updated list view to show one previous rec per day

--- app/src/main/java/com/example/cs125_project/PreviousRecommendations.java
+++ app/src/main/java/com/example/cs125_project/PreviousRecommendations.java
@@ -71,15 +71,16 @@ public class PreviousRecommendations extends AppCompatActivity {
             public void onDataChange(DataSnapshot snapshot) {
                 previous_rec_list.clear();
                 for (DataSnapshot ds : snapshot.getChildren()) {
-                    if (ds.child("best_rec").exists()) {
+                    if (ds.child("best_rec").exists())
+                    {
                         // ds.child("hourSlept").child(currentDate).child("best_rec").getValue())
                         best_rec = ds.child("best_rec").getValue(Integer.class);
                         better_rec = ds.child("better_rec").getValue(Integer.class);
                         mod_rec = ds.child("mod_rec").getValue(Integer.class);
                         String entry = getDateTime() + ": \n" + "Best: " + best_rec + ", Better: " + better_rec + ", Moderate: " + mod_rec;
-                        previous_rec_list.add(String.valueOf(entry));
-                        //previous_rec_list.add(String.valueOf(better_rec));
-                        //previous_rec_list.add(String.valueOf(mod_rec));
+                        if (!previous_rec_list.contains(entry)) {
+                            previous_rec_list.add(String.valueOf(entry));
+                        }
                         Log.v("FULL ARRAY", String.valueOf(previous_rec_list));
 //                    Log.v("best rec ", String.valueOf(previous_rec_list.get(0)));
 //                    Log.v("better rec ", String.valueOf(previous_rec_list.get(1)));
